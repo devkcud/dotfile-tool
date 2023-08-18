926681925dc036a206cf0b4a6301407a092019ab
André Luis (devkcud) <patommmmm@proton.me>
2023-08-18 15:47:55 -0300

    feat: Add command config
    Simply to get the current config

--- cmd/dotme/main.go
+++ cmd/dotme/main.go
@@ -21,6 +21,25 @@ func main() {
 	command := strings.ToLower(args[0])
 
 	switch command {
+	case "c", "config":
+		fmt.Println("Reading from:", config.Path)
+
+		subcommand := args[1:]
+		if len(subcommand) == 0 {
+			fmt.Printf("LookupEnv: $%s\nGitRemote: %s\nVerbose: %t\n", strings.Join(config.Current.LookupEnv, ", $"), config.Current.GitRemote, config.Current.Verbose)
+			return
+		}
+
+		switch subcommand[0] {
+		case "e", "env", "lookupenv":
+			fmt.Println("$" + strings.Join(config.Current.LookupEnv, ", $"))
+		case "g", "git", "gitremote":
+			fmt.Println(config.Current.GitRemote)
+		case "v", "verb", "verbose":
+			fmt.Println(config.Current.Verbose)
+		default:
+			fmt.Printf("error: Unknown subcommand: %s\n", subcommand[0])
+		}
 	default:
 		fmt.Printf("error: Unknown command: %s\n", command)
 	}

1b63bcc36302784108873a105f20dd77123cca77
André Luis (devkcud) <patommmmm@proton.me>
2023-08-18 15:12:55 -0300

    feat: Add config

--- cmd/dotme/main.go
+++ cmd/dotme/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/devkcud/dotfile-tool/internal/config"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	config.Setup()
+
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("error: Need at least one argument")
+		return
+	}
+
+	command := strings.ToLower(args[0])
+
+	switch command {
+	default:
+		fmt.Printf("error: Unknown command: %s\n", command)
+	}
 }
